13980be0f04cf89755a5d2415d2eb1a1528241f0
STR <saitarun004@gmail.com>
2023-04-29 00:30:56 +0530

    Initial Commit

---
+++ src/com/p1/Main.java
@@ -0,0 +1,16 @@
+package com.p1;
+
+public class Main {
+    public static void main(String[] args) {
+        recursion(1);
+    }
+
+    public static void recursion(int n){
+        if(n==5){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        recursion(n+1);
+    }
+}
